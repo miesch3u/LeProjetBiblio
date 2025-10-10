a990dda40bc57a3f833dc286f6041916ddb023cd
Egario-akier <reika.jacquot@gmail.com>
2025-10-10 10:20:34 +0200

    Push initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.idmc.projetbiblio {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.idmc.projetbiblio to javafx.fxml;
+    exports com.idmc.projetbiblio;
+}
